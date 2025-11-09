cc7a03ea8e69be7dc5730cf79cfa63cdf911e2b4
HuyBrox <128505633+HuyBrox@users.noreply.github.com>
2025-11-09 23:47:59 +0700

    commit

--- src/types/express.types.ts
+++ src/types/express.types.ts
@@ -1,6 +1,7 @@
 import { Request } from 'express';
 
 //cấu hình thêm thuộc tính user vào Request của express
+// File và files sẽ được tự động thêm bởi multer middleware
 export interface AuthRequest extends Request {
   user?: {
     id: string;
@@ -8,6 +9,7 @@ export interface AuthRequest extends Request {
     isAdmin: boolean;
     name: string;
   };
-  file?: Express.Multer.File;
-  files?: { [fieldname: string]: Express.Multer.File[] } | Express.Multer.File[];
+  // Multer adds file property, type sẽ được infer từ multer
+  file?: Express.Multer.File | undefined;
+  files?: { [fieldname: string]: Express.Multer.File[] } | Express.Multer.File[] | undefined;
 }
